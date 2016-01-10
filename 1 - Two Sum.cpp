1ad36931f5adc848aff97b4ced2f61540554765a
stoney <special.vcc@gmail.com>
2016-01-10 23:01:20 +0800

    Add solution for problems 1 and 2.

---
+++ 1 - Two Sum.cpp
@@ -0,0 +1,63 @@
+/*
+ =================================================================================== 
+ *       File Name: 1 - Two Sum.cpp
+ *     Description: https://leetcode.com/problems/two-sum/
+ *          Author: Stoney
+ * 
+ *         Version: 1.0
+ * Solution Method: sort the numbers, and binary search the vector for each number.
+ *     Submit Time: 2016/01/10
+ *        Run Time: 12ms, beats 85.88%
+ =================================================================================== 
+ */
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		int N = nums.size();
+		pair<int, int> nums2[N];
+		for(int i=0;i<N;i++){
+			nums2[i] = make_pair(nums[i], i+1);
+		}
+		
+		sort(nums2, nums2+N);
+		vector<int> res;
+		for(int i=0;i<N;i++){
+			pair<int, int> P = nums2[i];
+			int index = b_search(nums2+i, nums2+N, target-P.first);
+			if(index >=0){
+				res.push_back(min(P.second, index));
+				res.push_back(max(P.second, index));
+				return res;
+			}
+		}
+	}
+	
+	int b_search(pair<int, int> *begin, pair<int, int> *end, int v){
+		if(begin == end) return -1;
+		
+		pair<int, int> *middle = begin + (end - begin)/2;
+		pair<int, int> p = *middle;
+		if(p.first == v) return p.second;
+		else if(p.first < v) return b_search(middle+1, end, v);
+		else return b_search(begin, middle, v);
+	}
+	
+};
+
+int main(){
+	Solution s;
+	
+	vector<int> v;
+	v.push_back(2);
+	v.push_back(7);
+	v.push_back(11);
+	v.push_back(15);
+	v = s.twoSum(v, 9);
+	cout<<v[0]<<" "<<v[1]<<endl;
+	return 0;
+}
---
+++ 2 - Add Two Numbers.cpp
@@ -0,0 +1,49 @@
+/*
+ =================================================================================== 
+ *       File Name: 2 - Add Two Numbers.cpp
+ *     Description: https://leetcode.com/problems/add-two-numbers/
+ *          Author: Stoney
+ * 
+ *         Version: 1.0
+ * Solution Method: add the digits one by one alone with the linked list.
+ *     Submit Time: 2016/01/10
+ *        Run Time: 36ms, beats 70.83%
+ =================================================================================== 
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* res = new ListNode(0),* cur = res;
+        int p = 0;
+        
+        while(l1 || l2 || p){
+            int sum;
+            
+            if(l1 && l2) sum = l1->val + l2->val + p;
+            else if(l1) sum = l1->val + p;
+            else if(l2) sum = l2->val + p;
+            else sum = p;
+            
+            p = sum/10;
+            sum = sum%10;
+            
+            cur->next = new ListNode(sum);
+            cur = cur->next;
+            cur->next = NULL;
+            
+            l1 = l1?l1->next:NULL;
+            l2 = l2?l2->next:NULL;
+        }
+        
+        return res->next;
+    }
+};
