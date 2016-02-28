37e4e7c124dbb5720f9fc99c9771012f0feaad2f
stoney <special.vcc@gmail.com>
2016-02-28 20:46:11 +0800

    Add solution of #37

--- 238 - Product of Array Except Self.cpp
+++ 238 - Product of Array Except Self.cpp
@@ -7,32 +7,27 @@
  *          Note:
  *
  *       Version: 1.0
- *   Submit Time: 2016/02/12
- *      Run Time: 64ms, beats 15.10%
- *     Solutions: 首先计算乘积，然后逐个除以元素。但是注意数组中存在0的情况，若存在一个0，
- *                只有为0的位置不为0，其他部分为0；若有两个以上为0，全部为0.
+ *   Submit Time: 2016/02/27
+ *      Run Time: 60ms, beats 49.65%
+ *     Solutions: 动态规划：除去第i个数的乘积 = 第1到i-1元素的乘积 * i+1到n元素的；
+ *                两次遍历求出1到n的各个乘积、n到1的各个乘积
  ===================================================================================
  */
 
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans(nums.size(), 0);
+ class Solution {
+ public:
+     vector<int> productExceptSelf(vector<int>& nums) {
+         int prdFromStart[nums.size()+2];
+         int prdToEnd[nums.size()+2];
 
-        int gotZero = 0, pos;
-        long pro = 1;
-        for(int i=0;i<nums.size();++i) {
-            if(nums[i]==0){
-                ++gotZero; pos=i;
-            }
-            else pro*=nums[i];
-        }
+         prdFromStart[0] = 1;
+         for(int i=1;i<nums.size()+1;++i) prdFromStart[i] = prdFromStart[i-1]*nums[i-1];
 
-        if(gotZero==0){
-            for(int i=0;i<ans.size();++i) ans[i] = pro/nums[i];
-        }
-        else if(gotZero==1) ans[pos]=pro;
+         prdToEnd[nums.size()+1] = 1;
+         for(int i=nums.size();i>0;--i) prdToEnd[i] = prdToEnd[i+1]*nums[i-1];
 
-        return ans;
-    }
-};
+         vector<int> ans(nums.size(), 1);
+         for(int i=0;i<nums.size();++i) ans[i] = prdFromStart[i]*prdToEnd[i+2];
+         return ans;
+     }
+ };
--- 37 - Sudoku Solver.cpp
+++ 37 - Sudoku Solver.cpp
@@ -1,54 +1,61 @@
+/*
+ ===================================================================================
+ *     File Name: 37 - Sudoku Solver.cpp
+ *   Description: https://leetcode.com/problems/soduku-solver/
+ *        Author: Stoney
+ *      Category: 数独、DFS
+ *          Note:
+ *
+ *       Version: 1.0
+ *   Submit Time: 2016/02/28
+ *      Run Time: 8ms, beats 79.06%
+ *     Solutions: DFS：1.首先通过已确定的数字计算每个cell可选的数字（同行、列，九宫中出现
+ *                的数字不可以出现），当只有一个可选时确定该cell的数字，直到没有确定数字。
+ *                2.选择当前可选数字最少的cell，选择一个可选数字，填入，进行递归；若该数字
+ *                结果为false，选择下一数字，若全部可选数字结果都为false，则无解，返回。
+ ===================================================================================
+ */
 #include<iostream>
 #include<vector>
 #include<set>
+#include<bitset>
+#include<cmath>
 using namespace std;
 
 class Solution {
 public:
     int N = 9;
-    struct Poss{
+    void solveSudoku(vector<vector<char>>& board) {
         set<int> tmp = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        vector<set<int>> possNum(N*N, tmp);
-
-        struct Cand{bitset<N> c(0x1ff);}
-        vector<vector<Cand> > posCand(3*N, vector<Cand>(N));
-    }
-
-    void solveSudoku(vector<string>& board) {
-        Poss poss;
+        vector<set<int>> poss(N*N, tmp);
 
         int count=0;
         for(int i=0;i<N;++i)
             for(int j=0;j<N;++j)
                 if(board[i][j] != '.'){
                     ++count;
-                    poss.possNum[i*N+j].clear();
+                    poss[i*N+j].clear();
                     updatePoss(poss, i, j, board[i][j]);
                 }
 
         solve(board, poss, count);
     }
 
-    void updatePoss(Poss &poss, int i, int j, char ch){
+    void updatePoss(vector<set<int>> &poss, int i, int j, char ch){
         int pos = ch-'0';
-        //row i
-        for(int k=0;k<N;++k)
-            if(poss.possNum[i*N+k].erase(pos)){
-                poss.posCand[i][pos-1].set(k);
-                --cand[i][pos-1];
-            }
-        //column j
-        for(int k=0;k<N;++k)
-            poss.possNum[k*N+j].erase(pos);
-        //row i
         for(int k=0;k<N;++k){
+            //row i
+            poss[i*N+k].erase(pos);
+            //column j
+            poss[k*N+j].erase(pos);
+            //row i
             int x = i/3*3+k/3;
             int y = j/3*3+k%3;
-            poss.possNum[x*N+y].erase(pos);
+            poss[x*N+y].erase(pos);
         }
     }
 
-    bool solve(vector<string>& board, vector<set<int>> poss, int count){
+    bool solve(vector<vector<char>>& board, vector<set<int>> poss, int count){
         int last_cnt = 0;
         while(count != last_cnt){
             last_cnt = count;
@@ -69,31 +76,39 @@ public:
         }
 
         if(count == N*N) return true;
+
+		//进行dfs时，每次选取可能性最小的cell填充数字，使可行树最小
+        int min = N, min_ind;
         for(int i=0;i<N;++i){
             for(int j=0;j<N;++j){
                 int index = i*N+j;
-                if(poss[index].size() > 1){
-                    set<int> tmp_set(poss[index]);
-                    vector<string> tmp_board(board);
-
-                    for(set<int>::iterator it=tmp_set.begin();it!=tmp_set.end();++it){
-                        poss[index].clear();
-                        poss[index].insert(*it);
-                        if(solve(board, poss, count)) return true;
-                        board = tmp_board;
-                    }
-                    poss[index] = tmp_set;
+                if(poss[index].size()>1 && poss[index].size()<min){
+                    min = poss[index].size();
+					min_ind = index;
                 }
             }
         }
+
+		set<int> tmp_set(poss[min_ind]);
+		vector<vector<char>> tmp_board(board);
+
+		for(set<int>::iterator it=tmp_set.begin();it!=tmp_set.end();++it){
+			poss[min_ind].clear();
+			poss[min_ind].insert(*it);
+			if(solve(board, poss, count)) return true;
+			board = tmp_board;
+		}
         return false;
     }
 };
 
+
 int main(){
     Solution s;
     vector<string> v({"1...7..3.","83.6.....","..29..6.8","6....49.7",".9.....5.","3.75....4","2.3..91..",".....2.43",".4..8...9"});
+    //vector<string> v({"..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."});
     s.solveSudoku(v);
+    cout<<"-------------------------------------"<<endl;
     for(int i=0;i<9;++i){
 		for(int j=0;j<9;j++)
 			cout<<v[i][j]<<" ";
